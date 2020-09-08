56f5436e6d9a9fb4db4e33f2ce5d262f696abd99
harguilar <30771959+harguilar@users.noreply.github.com>
2020-09-08 20:45:36 +0100

    Piston-updated-Information

--- lib/helpers/const_global.dart
+++ lib/helpers/const_global.dart
@@ -1,3 +1,4 @@
+
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -62,3 +63,4 @@ class ConstGlobal {
     });
   }
 }
+
